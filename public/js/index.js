b9da7fc2607a2072cef8f8727c8d34e242dac0dc
gr4vity0 <joaquimdieudonne@gmail.com>
2019-02-13 12:28:20 +0100

    ajout espace connection html css

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,56 @@
+//Script pour changer en mode nuit (inversion blancs noirs avec bouttons)
+
 let bgHeader = document.getElementsByTagName('header')[0];
-console.log(bgHeader);
 
 let input1 = document.getElementById('btn-noir');
-console.log(input1);
 
 let input2 = document.getElementById('btn-blanc');
-console.log(input2);
 
 let testItem = document.querySelectorAll('.nav-link');
-let testItem2 = [...testItem];
+
 
 input1.addEventListener('click', () => {
     bgHeader.style.backgroundColor = "black"; 
     bgHeader.style.color = "white"; 
-    for(let i = 0; i < testItem2.length; i++){
-        testItem2[i].style.color = "white";
+    for(let i = 0; i < testItem.length; i++){
+        testItem[i].style.color = "white";
     }});
 
 input2.addEventListener('click', () => {
     bgHeader.style.backgroundColor = "white"; 
     bgHeader.style.color = "black";
-    for(let i = 0; i < testItem2.length; i++){
-        testItem2[i].style.color = "black";
+    for(let i = 0; i < testItem.length; i++){
+        testItem[i].style.color = "black";
     }});
+
+
+//script espace de connection et inscription
+
+
+// Epace de connection qui apparait
+
+let btnConnection = document.getElementById('btn-connection');
+
+let maDivInscription = document.getElementById('inscription-form');
+
+connection = () =>{
+    let maDiv = document.getElementById('test2');
+
+    maDiv.style.display = "none";
+
+    maDivInscription.style.display ="none"
+
+    if (maDiv.style.display === "none") {
+        maDiv.style.display = "block";
+    } else {
+        maDiv.style.display = "none";
+    }   
+}
+
+btnConnection.addEventListener('click', connection);
+
+
+//Espace d'inscription qui apparait et espace de connection qui disparait
+
+
 
