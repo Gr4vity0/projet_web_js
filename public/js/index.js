5580791ea8497085c2b548fc2b7c0b7fbb7b229e
gr4vity0 <joaquimdieudonne@gmail.com>
2019-02-20 12:56:37 +0100

    js scroll navbar et modal

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,9 @@
 //Script pour changer en mode nuit (inversion blancs noirs avec bouttons)
 
 let bgHeader = document.getElementsByTagName('header')[0];
-
 let input1 = document.getElementById('btn-noir');
-
 let input2 = document.getElementById('btn-blanc');
-
 let testItem = document.querySelectorAll('.nav-link');
-
 
 input1.addEventListener('click', () => {
     bgHeader.style.backgroundColor = "black"; 
@@ -24,3 +20,41 @@ input2.addEventListener('click', () => {
     }});
 
 
+//---------> script modal
+let connection = document.getElementById('modal-connection');
+let inscription = document.getElementById('modal-inscription');
+let bouttonInscription = document.getElementById('btn-inscription');
+let bouttonconnection = document.getElementById('btn-seconnecter');
+
+inscription.style.display = "none";
+
+//script modal boutton inscription
+bouttonInscription.addEventListener('click', () => {
+    connection.style.display = "none";
+    inscription.style.display = "block"
+})
+
+//script modal boutton connection
+bouttonconnection.addEventListener('click', () =>{
+    connection.style.display = "block";
+    inscription.style.display = "none";
+})
+
+
+
+//--------> script navbar
+let navbar = document.getElementById('maNav');
+let monh1 = document.getElementById('emporiumh1');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    navbar.className = 'navbar navbar-expand-lg fixed-top navbar-light transparent bg-light py-4';
+    monh1.className = 'text-left font-weight-bold' 
+
+  } else if (document.body.scrollTop < 500 || document.documentElement.scrollTop < 500){
+    navbar.className = 'navbar navbar-expand-lg navbar-light transparent';
+    monh1.className = 'text-left font-weight-bold d-none' 
+  }
+}
